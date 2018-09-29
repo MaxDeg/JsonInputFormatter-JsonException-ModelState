382b47acba724ff3fc59ca42b455162817a5a248
Max <max.degallaix@outlook.com>
2018-09-29 21:59:07 +0200

    first commit

---
+++ App/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace JsonInputFormatter_JsonException_ModelState.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("echo")]
+        public IActionResult Post([FromBody] Model value)
+        {
+            return Ok(value);
+        }
+    }
+
+    public class Model
+    {
+        [JsonProperty("id", Required = Required.Always)]
+        public int Id { get; set; }
+    }
+}
---
+++ Tests/Tests/Class1.cs
@@ -0,0 +1,51 @@
+﻿using JsonInputFormatter_JsonException_ModelState;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1 : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public Class1(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Test_ModelState()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var content = new StringContent("{ 'id': 2 }", Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("/echo", content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Test_ModelState_Error()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var content = new StringContent("{ }", Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("/echo", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("{\"id\":[\"Required property 'id' not found in JSON.Path '', line 1, position 3.\"]}", responseContent);
+        }
+    }
+}
