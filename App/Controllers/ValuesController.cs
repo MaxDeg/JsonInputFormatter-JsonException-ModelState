b90ed4b0c2218810c799e5b08e9ca79a4e6167a7
Max <max.degallaix@outlook.com>
2018-09-29 22:18:38 +0200

    Add url-encoded test

--- App/Controllers/ValuesController.cs
+++ App/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
 
 namespace JsonInputFormatter_JsonException_ModelState.Controllers
@@ -15,11 +17,18 @@ namespace JsonInputFormatter_JsonException_ModelState.Controllers
         {
             return Ok(value);
         }
+
+        [HttpPost("echo2")]
+        public IActionResult Post2([FromForm] Model value)
+        {
+            return Ok(value);
+        }
     }
 
     public class Model
     {
         [JsonProperty("id", Required = Required.Always)]
+        [Required, BindRequired]
         public int Id { get; set; }
     }
 }
--- Tests/Tests/Class1.cs
+++ Tests/Tests/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using JsonInputFormatter_JsonException_ModelState;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -46,6 +47,22 @@ namespace Tests
             // Assert
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
             Assert.Equal("{\"id\":[\"Required property 'id' not found in JSON.Path '', line 1, position 3.\"]}", responseContent);
+        }
+
+        [Fact]
+        public async Task Test_ModelState_Error_Url_Encoding()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>());
+            var response = await client.PostAsync("/echo2", content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("{\"Id\":[\"A value for the 'Id' property was not provided.\"]}", responseContent);
         }
     }
 }
